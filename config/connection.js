1174a56afb56e6599f0e967fe4a9a2f26da98db4
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-08-24 14:24:23 +0530

    MySQL Added

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("blog_db", "root", "tC]@2ZK5rz,J~lT", {
-  host: "localhost",
+const sequelize = new Sequelize("blog_db", "admin", "password333", {
+  host: "blogdb.cfwbui4fxbnc.eu-north-1.rds.amazonaws.com",
+  port: "3306",
   dialect: "mysql",
 });
 
