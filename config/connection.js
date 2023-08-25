5ebca6f7b85483b1b1dc9be7b6ff60d1eebb74e4
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-08-25 13:07:06 +0530

    DB

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("blogdb", "admin", "password888", {
-  host: "database-1.cfwbui4fxbnc.eu-north-1.rds.amazonaws.com",
-  port: "3306",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
